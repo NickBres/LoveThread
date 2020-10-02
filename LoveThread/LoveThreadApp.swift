2008cb70def2bae0ed3578590eaa98342ec02d02
Nikita Breslavsky <breslavsky97@gmail.com>
2020-10-02 10:42:18 +0300

    Initial Commit

---
+++ LoveThread/LoveThreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveThreadApp.swift
+//  LoveThread
+//
+//  Created by Nikita Breslavsky on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LoveThreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
